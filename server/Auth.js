f39d5decb658879e032d3cebfb34f0d5051685be
OhItsLena <lena.hammerer@vol.at>
2019-06-01 11:10:38 +0200

    Added authentication route

--- server/Auth.js
+++ server/Auth.js
@@ -1,24 +1,45 @@
 //Authentication
-let basicAuth = require('basic-auth');
-User = require('./UserModel')
+let basicAuth = require("basic-auth");
+User = require("./UserModel");
 
-exports.auth = function (req, res, next) {
-    var user = basicAuth(req);
-    if (!user || !user.name || !user.pass) {
-      res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+exports.initialAuth = function(req, res, next) {
+  var user = basicAuth(req);
+  if (!user || !user.name || !user.pass) {
+    res.set("WWW-Authenticate", "Basic realm=Authorization Required");
+    res.sendStatus(401);
+    return;
+  }
+
+  User.findOne({ name: user.name }, function(err, u) {
+    if (err) res.sendStatus(401);
+    if (user.name === u.name && user.pass === u.password) {
+      res.json({
+        data: u
+      });
+    } else {
+      res.set("WWW-Authenticate", "Basic realm=Authorization Required");
       res.sendStatus(401);
       return;
     }
-  
-    User.findOne({'name':user.name}, function(err, u){
-      if(err)
-          res.sendStatus(401);
-      if (user.name === u.name && user.pass === u.password) {
-          next();
-      } else {
-          res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-          res.sendStatus(401);
-          return;
-      }
-    })
+  });
+};
+
+exports.auth = function(req, res, next) {
+  var user = basicAuth(req);
+  if (!user || !user.name || !user.pass) {
+    res.set("WWW-Authenticate", "Basic realm=Authorization Required");
+    res.sendStatus(401);
+    return;
   }
+
+  User.findOne({ name: user.name }, function(err, u) {
+    if (err) res.sendStatus(401);
+    if (user.name === u.name && user.pass === u.password) {
+      next();
+    } else {
+      res.set("WWW-Authenticate", "Basic realm=Authorization Required");
+      res.sendStatus(401);
+      return;
+    }
+  });
+};
--- server/index.js
+++ server/index.js
@@ -1,29 +1,31 @@
 // Import express
-let express = require('express');
+let express = require("express");
 // Import Body parser
-let bodyParser = require('body-parser');
+let bodyParser = require("body-parser");
 // Import Mongoose
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 // Enable CORS
-let cors = require('cors');
+let cors = require("cors");
 // Initialize the app
 let app = express();
 
 // CORS
 app.use(cors());
 
-let authentication = require('./Auth')
+let authentication = require("./Auth");
 // Import routes
-let apiRoutes = require("./api-routes")
+let apiRoutes = require("./api-routes");
 
 // Configure bodyparser to handle post requests
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 app.use(bodyParser.json());
 
 // Connect to Mongoose and set connection variable
-mongoose.connect('mongodb://localhost/restaurant');
+mongoose.connect("mongodb://localhost/restaurant");
 
 var db = mongoose.connection;
 // Setup server port
@@ -32,12 +34,12 @@ var port = process.env.PORT || 8081;
 app.all("/api/*", authentication.auth);
 
 // Send message for default URL
-app.get('/', (req, res) => res.send('Hello World with Express'));
+app.get("/", (req, res) => res.send("Hello World with Express"));
 
 // Use Api routes in the App
-app.use('/api', apiRoutes)
+app.use("/api", apiRoutes);
 
 // Launch app to listen to specified port
-app.listen(port, function () {
-    console.log("Running RestaurantAPI on port " + port);
+app.listen(port, function() {
+  console.log("Running RestaurantAPI on port " + port);
 });
